650f7bfb42637a3c459b3d0db5626fbf4097ae85
1134923982 <1134923982@qq.com>
2019-07-11 14:55:10 +0800

    feat: implement mobile.java

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,47 @@
+public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+	public Mobile(String name, String color, String brand) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.brand = brand;
+	}
+
+    public void call(String message) {
+        System.out.println("Message : "+message);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Mobile [name=" + name + ", color=" + color + ", brand=" + brand + "]";
+    }
+
+
+}
